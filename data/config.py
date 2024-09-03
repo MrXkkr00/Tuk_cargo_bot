8c008394ab8ca91c9b402f9014185cd102b02bd0
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-09-03 12:43:00 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,16 +4,16 @@ IP = "localhost"
 
 
 
-# BOT_TOKEN = "7236139797:AAHSu8WmIqMwN8vKHC2SKmOakMO7novkgwI"
-# admin = "7139019580"   #mijoz idsi
-# DB_USER = "botlaru1_stg_user"
-# DB_PASS = "stg_user"
-# DB_NAME = "botlaru1_tuk_bargo_db_name"
+# BOT_TOKEN = ""
+# admin = ""   #mijoz idsi
+# DB_USER = ""
+# DB_PASS = ""
+# DB_NAME = ""
 # DB_HOST = "localhost"
 
 #
-BOT_TOKEN = "7490569949:AAGd8EWL0p5UbP8hka_xcmVgMFFtiEh4fw8"
-admin = "7010118152"   # mening idim
+BOT_TOKEN = ""
+admin = ""   # mening idim
 DB_USER = "postgres"
 DB_PASS = "12345ras"
 DB_NAME = "tuk_cargo"
